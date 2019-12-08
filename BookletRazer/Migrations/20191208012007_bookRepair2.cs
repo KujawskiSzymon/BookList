96d77f926233d74400c6dbb93fc279d0b2776012
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2019-12-08 10:36:38 +0100

    Add project files.

---
+++ BookletRazer/Migrations/20191208012007_bookRepair2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookletRazer.Migrations
+{
+    public partial class bookRepair2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+        }
+    }
+}
---
+++ BookletRazer/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookletRazer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookletRazer.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
