0c84dde95a7447a3ce66de48eaf244296b26a89e
KujawskiSzymon <42217160+KujawskiSzymon@users.noreply.github.com>
2019-12-08 11:03:00 +0100

    Edit page

---
+++ BookletRazer/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookletRazer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookletRazer.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext dbContext;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            dbContext = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await dbContext.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await dbContext.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+                await dbContext.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
